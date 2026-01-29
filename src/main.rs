2a3a34260e5a2b5d3da5012e2b6a6df17b97faeb
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2026-01-29 10:50:42 +0100

    ✨ feat: Add support for more Markdown features
    Enables several new Markdown features in the parser options:
    
    • Footnotes
    • Task lists
    • Heading attributes
    • Smart punctuation

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,11 @@ fn main() {
     let mut options = Options::empty();
     options.insert(Options::ENABLE_TABLES);
     options.insert(Options::ENABLE_STRIKETHROUGH);
-    
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+    options.insert(Options::ENABLE_SMART_PUNCTUATION);
+
     let parser = Parser::new_ext(&md_input, options);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
